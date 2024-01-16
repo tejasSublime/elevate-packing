4c2143892c961260be8745f4e92024686266856a
warlikeprinse <bhausaheb.sublimetechnocorp@gmail.com>
2024-01-16 11:28:17 +0530

    added new pages and packages

---
+++ app/login/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function layout({ children }) {
+    return (
+        <div>   <h2>Hiii login</h2>
+            {children}
+        </div>
+    )
+}
+
+export default layout
